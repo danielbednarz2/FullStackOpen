1bde0bf9c8fc36ab75536134875f250f8e20580c
Daniel <danielbednarz2@gmail.com>
2021-11-10 14:00:53 -0800

    Cleaning Code - Line Reduction & Ternary Operator

--- Part1/practice/complex-state-debugging/src/App.js
+++ Part1/practice/complex-state-debugging/src/App.js
@@ -1,33 +1,17 @@
 import { useState } from 'react'
 
-const History = ({ allClicks }) => {
-  if (allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div> 
-      button press history: {allClicks.join(' ')}
-    </div>
-  )
-}
+const History = ({ allClicks }) => allClicks.length === 0 ? 
+    <div>the app is used by pressing the buttons</div> :
+    <div>button press history: {allClicks.join(' ')}</div>
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0,
-    allClicks: []
-  })
+  const [clicks, setClicks] = useState({ left: 0, right: 0, allClicks: []})
 
   const handleLeftClick = () => setClicks({...clicks, left: clicks.left + 1, allClicks: clicks.allClicks.concat('L')})
   const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1, allClicks: clicks.allClicks.concat('R')})
   
-
   return (
     <div>
       {clicks.left}
