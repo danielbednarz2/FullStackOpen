25e1aa85a8859dba136cd4d9a8e3719272bdcddb
Daniel <danielbednarz2@gmail.com>
2021-11-10 12:03:19 -0800

    Component Helper Functions & Destructuring

---
+++ Part1/component-state-event-handlers/src/App.js
@@ -0,0 +1,43 @@
+const Hello = ({ name, age }) => {
+  /* Assigning props object
+  const name = props.name
+  const age = props.age
+  */
+
+  /* Destructuring props object inside component
+  const { name, age } = props
+  */
+
+  // Component Helper Function
+  const bornYear = () => new Date().getFullYear() - age 
+    /* Long Way for Arrow Function
+    { const yearNow = new Date().getFullYear()
+      return yearNow - age }
+    */
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>
+        So you were probably born in {bornYear()}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26+10} />
+      <Hello name={name} age={age} />
+    </div>
+  );
+}
+
+export default App;
