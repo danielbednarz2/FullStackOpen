b0542a4400d0c4043d5203210af0950ca85cce0e
Daniel <danielbednarz2@gmail.com>
2021-11-10 09:40:24 -0800

    courseinfo step2

--- Part1/courseinfo/src/App.js
+++ Part1/courseinfo/src/App.js
@@ -14,17 +14,21 @@ const App = () => {
   }
 
   const Content = () => {
+    const Part = (props) => {
+      return (
+        <div>
+          <p>
+            {props.part} {props.exercises}
+        </p>
+        </div>
+      )
+    }
+
     return (
       <div>
-        <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
+        <Part part={part1} exercises={exercises1}/>
+        <Part part={part2} exercises={exercises2}/>
+        <Part part={part3} exercises={exercises3}/>
       </div>
     )
   }
