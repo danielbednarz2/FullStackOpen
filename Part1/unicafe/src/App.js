b82dbb06dd6a34e6b9ddd89375fbe1940ed3f817
Daniel <danielbednarz2@gmail.com>
2021-11-10 15:10:03 -0800

    Exercise 1.10 & 1.11 - unicafe step5&6

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -13,23 +13,27 @@ const Feedback = ({ handleGood, handleNeutral, handleBad }) => {
   )
 }
 
-const Stats = ({ voteValue, text }) => <p>{text} {voteValue}</p>
+const Stats = ({ voteValue, text }) => <tr><td>{text}</td><td>{voteValue}</td></tr>
 
 const Statistics = ({ goodValue, neutralValue, badValue }) => {
   const allStats = goodValue + neutralValue + badValue
-  const averageStats = (allStats / 3) * .1
-  const positiveStats = (goodValue / allStats) * 100 + ' %'
+  const averageStats = Math.round(((allStats / 3) * .1 + Number.EPSILON) * 10) / 10
+  const positiveStats = Math.round(((goodValue / allStats) * 100 + Number.EPSILON) * 10) / 10 + ' %'
 
   if (goodValue || neutralValue || badValue > 0) {
     return (
       <>
         <h1>statistics</h1>
-        <Stats voteValue={goodValue} text='good'/>
-        <Stats voteValue={neutralValue} text='neutral'/>
-        <Stats voteValue={badValue} text='bad'/>
-        <Stats voteValue={allStats} text='all'/>
-        <Stats voteValue={averageStats} text='average'/>
-        <Stats voteValue={positiveStats} text='positive'/>
+        <table>
+          <tbody>
+            <Stats voteValue={goodValue} text='good'/>
+            <Stats voteValue={neutralValue} text='neutral'/>
+            <Stats voteValue={badValue} text='bad'/>
+            <Stats voteValue={allStats} text='all'/>
+            <Stats voteValue={averageStats} text='average'/>
+            <Stats voteValue={positiveStats} text='positive'/>
+          </tbody>
+        </table>
       </>
     )
   }
