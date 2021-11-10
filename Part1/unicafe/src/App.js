605f415db3a77cd3909b8e220fd86d4cbc4ebe60
Daniel <danielbednarz2@gmail.com>
2021-11-10 14:50:33 -0800

    Exercise 1.7 - unicafe step2

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -16,12 +16,19 @@ const Feedback = ({ handleGood, handleNeutral, handleBad }) => {
 const Stats = ({ voteValue, text }) => <p>{text} {voteValue}</p>
 
 const Statistics = ({ goodValue, neutralValue, badValue }) => {
+  const allStats = goodValue + neutralValue + badValue
+  const averageStats = (allStats / 3) * .1
+  const positiveStats = (goodValue / allStats) * 100 + ' %'
+
   return (
     <>
       <h1>statistics</h1>
       <Stats voteValue={goodValue} text='good'/>
       <Stats voteValue={neutralValue} text='neutral'/>
       <Stats voteValue={badValue} text='bad'/>
+      <Stats voteValue={allStats} text='all'/>
+      <Stats voteValue={averageStats} text='average'/>
+      <Stats voteValue={positiveStats} text='positive'/>
     </>
   )
 }
