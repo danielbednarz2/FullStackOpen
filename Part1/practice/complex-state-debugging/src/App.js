22dd52d1711ea60959439260a0ba9c2c18e3de9d
Daniel <danielbednarz2@gmail.com>
2021-11-10 13:34:44 -0800

    Arrays in state

--- Part1/practice/complex-state-debugging/src/App.js
+++ Part1/practice/complex-state-debugging/src/App.js
@@ -3,20 +3,21 @@ import { useState } from 'react'
 const App = () => {
   const [clicks, setClicks] = useState({
     left: 0,
-    right: 0
+    right: 0,
+    allClicks: []
   })
 
-  const handleLeftClick = () => setClicks({...clicks, left: clicks.left + 1})
-  const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1})
-  const handleBothClick = () => setClicks({left: clicks.left +1, right: clicks.right + 1})
+  const handleLeftClick = () => setClicks({...clicks, left: clicks.left + 1, allClicks: clicks.allClicks.concat('L')})
+  const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1, allClicks: clicks.allClicks.concat('R')})
+  
 
   return (
     <div>
       {clicks.left}
       <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleBothClick}>both</button>
       <button onClick={handleRightClick}>right</button>
       {clicks.right}
+      <p>{clicks.allClicks.join(' ')}</p>
     </div>
   );
 }
