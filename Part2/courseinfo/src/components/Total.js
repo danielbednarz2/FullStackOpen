2c5d5714e5f539acbd0142072d4022696f0e0f5a
Daniel <danielbednarz2@gmail.com>
2021-11-11 14:37:54 -0800

    Exercise 2.3 - courseinfo step8

--- Part2/courseinfo/src/components/Total.js
+++ Part2/courseinfo/src/components/Total.js
@@ -1,6 +1,5 @@
 const Total = ({parts}) => {
-    let total = 0
-    parts.map(part => total += part.exercises)
+    const total = parts.reduce(((pre, cur) => pre + cur.exercises),0)
     return <p>total of {total} exercises</p>
   }
 
