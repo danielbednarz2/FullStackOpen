16f20a4e1303074ed62e3af674f94556b2a33cb3
Daniel <danielbednarz2@gmail.com>
2021-11-12 11:10:59 -0800

    Exercise 2.8 - The Phonebook step3

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -2,53 +2,46 @@ import { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState ([
-    { name: 'Arto Hellas', id: 1 }
+    { name: 'Arto Hellas', id: 1, number: 1234567890 }
   ])
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  
+  let hasVal = false;
 
   const handleAddNumber = (e) => {
     e.preventDefault()
     
     const newPerson = {
       name: newName,
-      id: persons.length + 1
+      id: persons.length + 1,
+      number: newNumber,
     }
 
-    let hasVal = false;
-
-    for (let key in persons) { 
-      if (persons[key].name === newPerson.name) {
-         hasVal = true;
-         break;
-      }}
+    for (let key in persons) {persons[key].name === newPerson.name ? hasVal = true : hasVal = false}
       
-    if (!hasVal) 
-    {
-      setPersons(persons.concat(newPerson))
-    } else {
-      alert(`${newName} is already added to phonebook`)
-    }
+    !hasVal ? setPersons(persons.concat(newPerson)) : alert(`${newName} is already added to phonebook`)
+
     setNewName('') 
+    setNewNumber('')
   }
   
   const handleNameInput = (e) => setNewName(e.target.value)
+  const handleNumberInput = (e) => setNewNumber(e.target.value)
 
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
-        <div>
-          name: <input value={newName} onChange={handleNameInput} />
-        </div>
-        <div>
-          <button onClick={handleAddNumber} type="submit">add</button>
-        </div>
+        <div>name: <input value={newName} onChange={handleNameInput} /></div>
+        <div>number: <input value={newNumber} onChange={handleNumberInput} /></div>
+        <div><button onClick={handleAddNumber} type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       {persons.map(person => 
         <p key={person.id}>
-          {person.name}
+          {person.name} {person.number}
         </p>
         )}
     </div>
