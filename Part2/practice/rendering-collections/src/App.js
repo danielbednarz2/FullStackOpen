7aef06c1a2af042136e06a01cb3005b032f9e49d
Daniel <danielbednarz2@gmail.com>
2021-11-11 16:04:49 -0800

    Practice 2b Forms

--- Part2/practice/rendering-collections/src/App.js
+++ Part2/practice/rendering-collections/src/App.js
@@ -1,14 +1,45 @@
 import Note from './components/Note'
+import { useState } from 'react'
 
 const App = ({ notes }) => {
+  const [note, setNote] = useState(notes)
+  const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (e) => {
+    e.preventDefault()
+    const noteObject = {
+      id: note.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+    }
+
+    setNote(note.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleValueChange = (e) => setNewNote(e.target.value)
+
+  const notesToShow = showAll ? note : note.filter(note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleValueChange}/>
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 }
