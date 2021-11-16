2180be11a852ffc4afc7e46ec09c8034e83cc3b3
Daniel <danielbednarz2@gmail.com>
2021-11-15 16:32:44 -0800

    Exercise 2.14 - Data for countries step3

--- Part2/data-for-countries/src/App.js
+++ Part2/data-for-countries/src/App.js
@@ -3,15 +3,19 @@ import axios from 'axios'
 import Search from './components/Search'
 import Countries from './components/Countries'
 import Country from './components/Country'
+import dotenv from 'dotenv'
 
 const App = () => {
     const [ country, setCountry ] = useState([])
     const [ search, setSearch ] = useState('')
 
+    dotenv.config()
+    const weatherAPI=process.env.REACT_APP_API_KEY_WEATHER
+
     useEffect(() => {
-        axios
-        .get('https://restcountries.com/v3.1/all')
-        .then(res => setCountry(res.data))
+            axios
+            .get('https://restcountries.com/v3.1/all')
+            .then(res => setCountry(res.data))
     }, [])
 
     let filteredCountries = country.filter(place => place.name.common.toLowerCase().includes(search.toLowerCase()))
@@ -25,7 +29,7 @@ const App = () => {
             filteredCountries.length > 10 ? 
                 <p>Too many matches, specify country</p> : 
             filteredCountries.length === 1 ?
-                <Country filteredCountries={filteredCountries} /> :
+                <Country filteredCountries={filteredCountries} weatherAPI={weatherAPI} /> :
                 <Countries filteredCountries={filteredCountries} setSearch={setSearch} />
             }
         </div>
--- Part2/data-for-countries/src/components/Country.js
+++ Part2/data-for-countries/src/components/Country.js
@@ -1,7 +1,21 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 
-const Country = ({ filteredCountries }) => {
+const Country = ({ filteredCountries, weatherAPI }) => {
+    const [ temp, setTemp ] = useState('')
+    const [ wind, setWind ] = useState('')
     let languageArray = []
     let id = 0
+
+    useEffect(() => {
+        axios
+        .get(`http://api.openweathermap.org/data/2.5/weather?q=${filteredCountries[0].capital}&appid=${weatherAPI}
+        `)
+        .then(res => {
+            setTemp(res.data.main.temp)
+            setWind(res.data.wind.speed)
+        })
+    }, [filteredCountries, weatherAPI])
 
     for (let key in filteredCountries[0].languages) {
         languageArray.push(filteredCountries[0].languages[key])
@@ -17,6 +31,9 @@ const Country = ({ filteredCountries }) => {
             {languageArray.map(lang => <li key={id++}>{lang}</li>)}
         </ul>
         <img src={`${filteredCountries[0].flags.svg}`} alt={`${filteredCountries[0].name.common} flag`} height={100} width={"auto"} />
+        <h2>weather</h2>
+        <p>temparture: {Math.round((temp - 273.15) * (9/5) + 32)} degrees Farenheit</p>
+        <p>wind: {Math.round(wind * 1.609)} mph</p>
         </>
     )
 }
